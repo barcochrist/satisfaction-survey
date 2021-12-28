4f7ed22daad5c1bd450437e298d8592a45ff5b96
Christian Barco <barcochrist@gmail.com>
2021-12-28 18:58:05 -0500

    (init) create basic project structure

---
+++ src/main/java/com/github/barcochrist/satisfactionsurvey/SatisfactionSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.github.barcochrist.satisfactionsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatisfactionSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatisfactionSurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/barcochrist/satisfactionsurvey/SatisfactionSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.barcochrist.satisfactionsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatisfactionSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
